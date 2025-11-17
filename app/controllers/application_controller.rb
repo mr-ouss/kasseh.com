6058075cf11e26aeaa279137599405b69f34c4b0
mr-ouss <q@kasseh.com>
2025-11-17 11:35:07 -0500

    linting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-  
+
   # Skip CSRF verification for OAuth callbacks
-  protect_from_forgery with: :exception, unless: -> { request.path.start_with?('/auth/') }
+  protect_from_forgery with: :exception, unless: -> { request.path.start_with?("/auth/") }
 end
